3bbc5ca7f4a14cca445217e163f6a7cd11063f92
newffnow <1574746932@qq.com>
2022-11-28 10:36:43 +0800

    first commit

---
+++ GD32F4xx_Firmware_Library_V2.1.3/Template/config.h
@@ -0,0 +1,17 @@
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#include <stdint.h>
+
+/* Program major version (before the '.') as a number */
+#define ZBAR_VERSION_MAJOR 0
+
+/* Program minor version (after '.') as a number */
+#define ZBAR_VERSION_MINOR 10
+
+#define 	PRIx32   "lx"
+
+
+#endif
+
+
